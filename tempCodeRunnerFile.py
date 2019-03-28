5ddc7832be3885b7498e5abbb336a4ac2fa63ddf
Eberhard Schneider <mail@eberhardschneider.info>
2019-03-28 11:06:41 +0100

    add vectorize whole chorale and display in mathplotlib

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def vectorizePart(part):
     lengthInQuarters = part.duration.quarterLength
     lengthIn16th = lengthInQuarters * 4
-    sequence = np.zeros((int(lengthIn16th), 127))
+    sequence = np.zeros((127, int(lengthIn16th)))
     measures = part.getElementsByClass('Measure')
     for measure in measures:
         for note in measure.notesAndRests:
@@ -13,37 +13,25 @@ def vectorizePart(part):
             pitch = note.pitch.midi
             duration = int(note.duration.quarterLength * 4)
             for i in range(duration):
-                sequence[offset + i, pitch] = 1
+                sequence[pitch, offset + i] = 1
     return sequence
 
 def vectorizeChorale(chorale):
-    result = []
+    chorale = []
     for part in chorale.parts:
-        result.append(vectorizePart(part))
-    return result
-
-def mergeBitVectors(vectors):
-    result = np.zeros((len(vectors[0]), 127 ))
-    for v in range(len(vectors)):
-        for i in range(len(vectors[v])):
-            for j in range(127):
-                result[i][j] = v+1 if vectors[v][i][j] != 0 else result[i][j]
-    return result
+        chorale.append(vectorizePart(part))
+    return chorale
 
 iterator =  music21.corpus.chorales.Iterator(numberingSystem='riemenschneider')
 # for chorale in iterator:
 #     print(chorale.parts[0].partName)
 
-chorale = vectorizeChorale(iterator[100])
-result = mergeBitVectors(chorale)
-result = np.rot90(result)
+chorale = vectorizeChorale(iterator[1])
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.imshow(result, aspect='auto', cmap=plt.cm.get_cmap('Greys'), interpolation='nearest')
-plt.show()  
-
-    
+ax.imshow(chorale, aspect='auto', cmap=plt.cm.get_cmap('Greys'), interpolation='nearest')
+plt.show()        
 
 # Tensor 4D:
 # 1. samples: alle 368 bach chorales
